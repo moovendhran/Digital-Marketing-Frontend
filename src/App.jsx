cd6e97a4b3eb1b8a335f1f5ac613903d1714956f
moovendhran <moovendhranofficial@gmail.com>
2026-05-30 15:29:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <App />
-);
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Brands from "./components/Brands";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
+import Values from "./components/Values";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+
+import Blog from "./pages/Blog";
+import Admin from "./pages/Admin";
+
+
+
+// Home page layout
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      <Brands />
+      <About />
+      <Services />
+      <Contact />
+      <Values />
+      <Testimonials />
+      <Admin/>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/Admin" element={<Admin/>}/>
+        <Route path="/Contact" element={<Contact />} />
+
+
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
+}
