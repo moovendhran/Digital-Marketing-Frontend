edbf6f24703a5b5209f8bd114db618a117d75bb7
moovendhran <moovendhranofficial@gmail.com>
2026-05-30 15:28:54 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,7 @@
-require("dotenv").config();
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
-mongoose.set("bufferCommands", false); // 🔥 IMPORTANT
-
-const app = express();
-
-app.use(cors({ origin: "http://localhost:5173" }));
-app.use(express.json());
-
-// ROUTES
-app.use("/api/digital", require("./routes/digitalRoutes"));
-app.use("/api/contact", require("./routes/contactRoutes"));
-
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(5000, () =>
-      console.log("Server running on port 5000")
-    );
-  })
-  .catch((err) => {
-    console.error("MongoDB connection failed:", err.message);
-  });
+createRoot(document.getElementById("root")).render(
+  <App />
+);
